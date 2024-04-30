7c94795b6194635f4f0ebe1a1b958c037bb2000c
akamesnow <valentin.folliot.vf@gmail.com>
2024-04-30 16:35:30 +0200

    wxdcwxc

---
+++ Application1/Application1/Behaviors/NavigationViewHeaderMode.cs
@@ -0,0 +1,8 @@
+﻿namespace Application1.Behaviors;
+
+public enum NavigationViewHeaderMode
+{
+    Always,
+    Never,
+    Minimal
+}
---
+++ Application1/Application1/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Application1.ViewModels;
+
+public partial class MainViewModel : ObservableRecipient
+{
+    public MainViewModel()
+    {
+    }
+}
